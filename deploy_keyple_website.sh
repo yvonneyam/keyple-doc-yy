9dde4cdcfa5967d473a7eb3d05b54ca51c1f765a
Jean-Pierre Fortune <jean-pierre.fortune@ialto.com>
2020-03-03 13:38:07 +0100

    Updated deploy script

--- deploy_keyple_website.sh
+++ deploy_keyple_website.sh
@@ -12,8 +12,8 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out website-published branch into public"
-git worktree add -B website-published public origin/website-published
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
@@ -21,8 +21,8 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
-echo "Updating website-published branch"
-cd public && git add --all && git commit -m "Publishing to website-published (deploy_keyple_website.sh)"
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (deploy_keyple_website.sh)"
 
 echo "Pushing to github"
 git push --all
